90ef1176fb3d82d287ae35903a516d3be9405108
myla steiger <mrs.steiger@gmail.com>
2022-02-01 16:28:19 -0500

    Add project files.

---
+++ array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace array {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+            int[] ages = {45, 41, 25, 23, 20, 19, 18};
+
+            int sum = 0;
+            for(int i = 0; i < ages.Length; i++ ) {
+                sum = sum + ages[i];
+            }
+            sum = 0;
+            foreach(int age in ages) {
+                sum = sum + age;
+            }
+            Console.WriteLine($"sum is {sum}");
+
+        }
+    }
+}
